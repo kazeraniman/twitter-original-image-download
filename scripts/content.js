e1012f17193cff7e05ca3be08e551679647586f7
Iman Kazerani <Kazeraniman@gmail.com>
2018-06-20 23:09:20 -0400

    Adding an element to tweets with images for the first set of tweets fetched by Twitter.

---
+++ scripts/content.js
@@ -0,0 +1,31 @@
+/**
+ * Adds an icon to enable downloading the image for tweets with images.
+ *
+ * Takes a tweet and determines whether or not it contains images.  If so, inject a new element
+ * in the action bar to facilitate downloading the full, original image.
+ *
+ * @param {Object} tweet    A list element tweet.
+ */
+function addDownloadLink(tweet) {
+    // Only deal with the tweets with images in them
+    let images = tweet.querySelectorAll(".AdaptiveMedia-container img");
+    if (images.length == 0) {
+        return;
+    }
+
+    // Create the download link element
+    let downloadLinkElement = document.createElement('img');
+    downloadLinkElement.src = chrome.runtime.getURL("resources/twitter_download_icon.png");
+
+    // Get the action bar
+    let actionBar = tweet.querySelector(".ProfileTweet-actionList");
+
+    // Add in the new element
+    actionBar.appendChild(downloadLinkElement);
+}
+
+// Get the tweet container
+let tweetsContainer = document.getElementById("stream-items-id");
+
+// Add the download link to all current tweets
+Array.from(tweetsContainer.children).forEach(tweet => addDownloadLink(tweet));
